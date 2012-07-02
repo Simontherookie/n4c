5d4605b9185e9ed3ae6ea3c06d6820988d82aedc
Nick Malcolm <nick@southgatelabs.com>
2012-07-02 19:24:38 +1200

    Adds guest validations and tests

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,7 @@
 class Guest < ActiveRecord::Base
+  
+  validates :name, presence: {allow_blank: false}
+  validates :email, presence: {allow_blank: false}
+  
   attr_accessible :address, :email, :name
 end
--- test/unit/guest_test.rb
+++ test/unit/guest_test.rb
@@ -2,4 +2,18 @@ require 'test_helper'
 
 class GuestTest < ActiveSupport::TestCase
   
+  test "factory is valid" do
+    assert FactoryGirl.build(:guest).valid?
+  end
+  
+  test "guest requires name" do
+    assert FactoryGirl.build(:guest, name: nil).invalid?
+    assert FactoryGirl.build(:guest, name: "").invalid?
+  end
+  
+  test "guest requires email" do
+    assert FactoryGirl.build(:guest, email: nil).invalid?
+    assert FactoryGirl.build(:guest, email: "").invalid?
+  end
+  
 end
