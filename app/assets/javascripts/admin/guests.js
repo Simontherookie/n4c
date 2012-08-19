866d29a5886cf13ab6fd9642a55c54b5b5e08774
Nick Malcolm <nick@southgatelabs.com>
2012-08-19 15:26:45 +1200

    Add some guest stats

--- app/assets/javascripts/admin/guests.js
+++ app/assets/javascripts/admin/guests.js
@@ -4,16 +4,30 @@ jQuery(function($) {
     var going_to_reception = $(this).find("input[name='guest[going_to_reception]']");
     var button = $(this).find("input.btn");
 
+    var going_count_h2 = $("h2[data-role='going-to-reception-count']");
+    var not_going_count_h2 = $("h2[data-role='not-going-to-reception-count']");
+
+    var going_count = parseInt(going_count_h2.html());
+    var not_going_count = parseInt(not_going_count_h2.html());
+
     if(going_to_reception.val() == "true"){
       button.removeClass("btn-danger");
       button.addClass("btn-success");
       button.val("Invited");
+      
       going_to_reception.val("false");
+
+      going_count_h2.html(going_count + 1);
+      not_going_count_h2.html(not_going_count - 1);
     }else{
       button.removeClass("btn-success");
       button.addClass("btn-danger");
       button.val("Not invited");
-      going_to_reception.val("true");      
+
+      going_to_reception.val("true");
+
+      going_count_h2.html(going_count - 1);
+      not_going_count_h2.html(not_going_count + 1);     
     }
   })
 
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -4,5 +4,8 @@ class Guest < ActiveRecord::Base
   validates :address, presence: {allow_blank: false}
   validates :email, presence: {allow_blank: false}
   
+  scope :going_to_reception, where(:going_to_reception => true)
+  scope :not_going_to_reception, where("going_to_reception != ?", true)
+
   attr_accessible :address, :email, :name
 end
