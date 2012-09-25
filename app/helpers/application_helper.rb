e7e9ac8c8cb5c3f4ccdcfb9d9b417eda5728636a
Nick Malcolm <nick@southgatelabs.com>
2012-09-25 23:38:18 +1200

    Add coming soon message to accomodation and gift registry links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-  def nav_link title, path
+  def nav_link title, path, options = {}
     content_tag :li, :class => ("active" if request.path.eql?(path)) do
-      link_to title, path
+      link_to title, path, options
     end
   end
 
