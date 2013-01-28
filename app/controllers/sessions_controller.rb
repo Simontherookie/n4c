d5ec5915e1c2fe50499fcbf0eee0382e445043da
Nick Malcolm <nick@southgatelabs.com>
2013-01-28 13:46:16 +1300

    Shut down sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,15 @@ class SessionsController < ApplicationController
   skip_before_filter :require_login
 
   def new
-    #Log them out
     session[:guest_id] = nil
-    
-    @session = Session.new(:email=>params[:email].try(:downcase))
+    flash[:error] = "Sorry, this website is now closed."
+    redirect_to root_path
   end
 
   def create
-    @session = Session.new(params[:session])
-    if guest = @session.guest
-      session[:guest_id] = guest.id
-      flash[:notice] = "Welcome, #{guest.name}!"
-      redirect_to rsvp_path
-    else
-      flash[:error] = "Sorry, you need to email rsvp@nickandchelsea2013.com before you can log in."
-      redirect_to root_path(:email => @session.email)
-    end
+    session[:guest_id] = nil
+    flash[:error] = "Sorry, this website is now closed."
+    redirect_to root_path
   end
 
   def destroy
